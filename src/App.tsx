b446e43bed6499ab9375d0f4d2b85647050bad80
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-11-13 15:01:36 -0300

    Ajustando header e adicionando swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 
 import GlobalStyles from './styles/GlobalStyles';
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Home />
     </>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../../components/Header";
 
 import { Container } from "./styles";
 
 const Home: React.FC = () => {
     return (
         <>
-            <Header />
             <Container></Container>
         </>
     );
